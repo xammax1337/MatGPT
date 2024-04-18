49b3487d0c821f558c3fa7b0ddf8ac2bc1ba945e
Quynh-Truong <146139597+Quynh-Truong@users.noreply.github.com>
2024-04-18 15:51:34 +0200

    Edited in GenerateRecipe and created database

---
+++ MatGPT/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using MatGPT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatGPT.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Credential> Credentials { get; set; }
+        public DbSet<FoodItem> FoodItems { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<FoodPreference> FoodPreferences { get; set; }
+        public DbSet <KitchenSupply> KitchenSupply { get; set;}
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
---
+++ MatGPT/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MatGPT.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Recipe> Recipes { get; set; }
+        public ICollection<FoodItem> FoodItems { get; set; }
+        public ICollection<FoodPreference> FoodPreferences { get; set;}
+        public ICollection<KitchenSupply> KitchenSupplies { get; set;}
+        public Credential Credential { get; set; }
+    }
+}
