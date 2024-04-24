6db1e70bc56d8945f0972dad6609dfdadc517cc4
Parmis Morshedi <parmis76.morshedi@gmail.com>
2024-04-24 13:20:31 +0200

    Updated DB with Pantry and PantryFoodItem

---
+++ MatGPT/Models/PantryFoodItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MatGPT.Models
+{
+    public class PantryFoodItem
+    {
+        [Key]
+        public int PantryFoodItemId { get; set; }
+
+        [ForeignKey("FoodItem")]
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; }
+
+        [ForeignKey("Pantry")]
+        public int PantryId { get; set; }
+        public Pantry Pantry { get; set; }
+      
+    }
+}
